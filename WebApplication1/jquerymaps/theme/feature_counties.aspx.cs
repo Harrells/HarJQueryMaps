4b29611d86362340c768ddffbeacffc37e428133
CaptainBly <paul@phoendev.com>
2023-01-21 08:23:14 -0500

    fix(states):state map hadn't been converted yet

--- WebApplication1/jquerymaps/theme/feature_counties.aspx.cs
+++ WebApplication1/jquerymaps/theme/feature_counties.aspx.cs
@@ -3,69 +3,64 @@ using System.Data.SqlClient;
 using System.Collections;
 using Dapper;
 using System.Linq;
-
+using WebApplication1.Models;
+using System.Collections.Generic;
 
 public partial class jquerymaps_theme_feature_counties : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        //Response.Write("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
-        Response.Write("<jqm_features xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"../../xsd/jqm_features.xsd\">\n");
+        try
+        {
+            using (SqlConnection db = new SqlConnection("Data Source=harazuresql1.database.windows.net;Initial Catalog=HarMaps;User ID=HarMapsUserFull;Password=YouCanTunaAPianoButYouCantTunaFish!135"))
+            {
 
-        String id = ""; if (Request["id"] != null) { id = Request["id"].ToString(); }
 
-        //MTW012023
+                Response.Write("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+                Response.Write("<jqm_features xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"../../xsd/jqm_features.xsd\">\n");
 
-                 //database dbAdmin = new database();
-            //dbAdmin.dbConnect();
+                String id = ""; if (Request["id"] != null) { id = Request["id"].ToString(); }
 
-            //ArrayList ranges = new ArrayList();
-            //String sql = "SELECT rangeID, range_min, range_max FROM bag_ranges WHERE range_level = 'county' ORDER BY rangeID";
-            //SqlDataReader rd = dbAdmin.sqlOpen(sql);
+                //MTW012023
 
-            //if (rd.HasRows)
-            //    while (rd.Read())
-            //    {
-            //        String[] element = new String[3];
-            //        element[0] = rd["rangeID"].ToString();
-            //        element[1] = rd["range_min"].ToString();
-            //        element[2] = rd["range_max"].ToString();
-            //        ranges.Add(element);
-            //    }
-            //dbAdmin.sqlClose();
+                String sql = "SELECT rangeID, range_min, range_max FROM bag_ranges WHERE range_level = 'county' ORDER BY rangeID";
+                List<Ranges> rangedata = db.Query<Ranges>(sql).ToList();
 
-            //sql = "SELECT c.countryID, c.stateID, c.countyID, c.county, c.CurSalesTotal AS total ";
-            //sql += "FROM jqm_us_countiesNew c ";
-            //sql += "WHERE c.stateID = '" + id + "' ";
-            ////sql += "AND c.courses > 0";
-            //sql += "ORDER BY c.countyID";
-            //rd = dbAdmin.sqlOpen(sql);
 
-            //if (rd.HasRows)
-            //    while (rd.Read())
-            //    {
+                sql = "SELECT c.countryID, c.stateID, c.countyID, c.county, c.CurSalesTotal AS total "
+                    + "FROM jqm_us_countiesNew c "
+                    + "WHERE c.stateID = '" + id + "' "
+                    + "ORDER BY c.countyID";
+                var results = db.Query(sql).ToList();
+                String category_str = "";
 
-            //        String category_str = "";
-            //        Double msa_value = Convert.ToDouble(rd["total"]);
+                foreach (var rd in results)
+                {
 
-            //        foreach (string[] element in ranges)
-            //        {
-            //            if (msa_value >= Convert.ToDouble(element[1]) && msa_value <= Convert.ToDouble(element[2]))
-            //                category_str = "feature_" + element[0];
-            //            else
-            //                if (msa_value >= Convert.ToDouble(element[1]) && Convert.ToDouble(element[2]) == 0)
-            //                    category_str = "feature_" + element[0];
-            //        }
+                    category_str = "";
+                    Double msa_value = Convert.ToDouble(rd.total);
 
-            //        Response.Write("<feature id=\"" + rd["countryID"].ToString() + "_" + rd["stateID"].ToString() + "_" + rd["countyID"].ToString() + "\" category=\"" + category_str + "\" label=\"" + Server.HtmlEncode(rd["county"].ToString()) + "\" popup=\"" + Convert.ToDouble(rd["total"].ToString()).ToString("##,##0") + "\" >\n");
-            //        // made change from county to rep so that rep shows up where county does when you zoom to state.
-            //        //Response.Write("<feature id=\"" + rd["countryID"].ToString() + "_" + rd["stateID"].ToString() + "_" + rd["countyID"].ToString() + "\" category=\"" + category_str + "\" label=\"" + Server.HtmlEncode(rd["county"].ToString()) + "\" popup=\"" + Convert.ToDouble(rd["total"].ToString()).ToString("##,##0") + "\" Sales=\"" + Convert.ToDouble(rd["CurSalesTotal"].ToString()).ToString("##,##0") + "\" salesRep=\"" + rd["rep"].ToString() + "\" >\n");
-            //        Response.Write("<action event=\"onClick\" target=\"loadChild\" url=\"us_county.aspx?id=##id##\" />");
-            //        Response.Write("</feature>");
-            //    }
-            //dbAdmin.sqlClose();
-            //dbAdmin.dbClose();
+                    var range = rangedata.Where(o => o.range_min <= msa_value && o.range_max >= msa_value).FirstOrDefault();
+                    if (range != null)
+                        category_str = "county_" + range.rangeID;
 
-            Response.Write("</jqm_features>");
+                    if (category_str != "")
+                    {
+                        Response.Write("<feature id=\"" + rd.countryID.ToString() + "_" + rd.stateID.ToString() + "_" + rd.countyID.ToString() + "\" category=\"" + category_str + "\" label=\"" + Server.HtmlEncode(rd.county.ToString()) + "\" popup=\"" + Convert.ToDouble(rd.total.ToString()).ToString("##,##0") + "\" >\n");
+                        // made change from county to rep so that rep shows up where county does when you zoom to state.
+                        //Response.Write("<feature id=\"" + rd["countryID"].ToString() + "_" + rd["stateID"].ToString() + "_" + rd["countyID"].ToString() + "\" category=\"" + category_str + "\" label=\"" + Server.HtmlEncode(rd["county"].ToString()) + "\" popup=\"" + Convert.ToDouble(rd["total"].ToString()).ToString("##,##0") + "\" Sales=\"" + Convert.ToDouble(rd["CurSalesTotal"].ToString()).ToString("##,##0") + "\" salesRep=\"" + rd["rep"].ToString() + "\" >\n");
+                        Response.Write("<action event=\"onClick\" target=\"loadChild\" url=\"us_county.aspx?id=##id##\" />");
+                        Response.Write("</feature>");
+                    }
+                }
+
+                Response.Write("</jqm_features>");
+            }
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+
     }
 }
